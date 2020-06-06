38b28b4b213ba494308fd0cffa01b14728c17fa7
Igor Ilic <igorilicbl@gmail.com>
2020-06-06 16:30:30 +0200

    Added support for virtual hosts

--- blog.md
+++ blog.md
@@ -1,14 +1,66 @@
-# Shel scripts for starting new projects
+---
+title: Shell scripts for starting new projects
+published: false
+description: Custom made shell script for starting new project
+tags: shell, bash, scripting
+---
 
-Recently I realized that I do a lot of repetitive steps when starting a new project. So I started to wander if there was a way to automate that process.
+Recently I realized that I do a lot of repetitive steps when starting a new project. So I started to think if there was a way to automate that process.
 
-Of course, I could have done this in a lot of different languages such as Python, C, PHP or any other, but I decided I wanted to do this in shel script (bash).
+Of course, I could have done this in a lot of different languages such as Python, C, PHP or any other, but I decided I wanted to do this in shell script (bash).
 
 Now some of you might be asking yourself "But why would you do that in bash?", and my answer to you is why wouldn't I. 
 
 I've done some minor stuff with it before but never really gotten to deep into learning it or truly trying to find out how it all works. I always viewed it as something you would see in movies when they show those "hacking" scenes. 
    
+   
 ![Man sitting behind a computer and typing on a keyboard](https://media.giphy.com/media/l46C6sdSa5DVSJnLG/giphy.gif "Scene from the Hackerman movie") 
 
-I always admired people who could write and understood what it does. So I decide the best way to learn it was to get my hands dirty with it. I started by trying to come up with an idea for what the script should do some basic concepts. And that is how all of this started. 
 
+I always admired people who could write and understood what it does. So I decide the best way to learn it was to get my hands dirty with it. I started by trying to come up with an idea for what the script should do and coming up with some basic concepts, and that is how all of this started. 
+
+### So what are the steps I usually take when starting a new project?
+ 
+* Give the project a name
+* Create the project folder
+* Initialize the folder as git repository
+    * Add git repo url
+* Create a database for the project
+* Create virtual host setup
+* Depending on the language of choice set up additional things, such as:
+    * composer
+    * npm
+
+
+So I started to lookup up documentation and tutorials on creating shell scripts and managed to create one I'm ok with using in my daily work.
+
+The basic usage of the script:
+
+```
+create-project.sh <project-name>
+```
+
+or you can use it with arguments:
+
+```
+create-project.sh <project-name> [-h | -p | -g | -e | -l | -q]
+```
+
+### Explanation of arguments
+
+You can pass all or none of the arguments as they all have predefined default values that will be used.
+
+* **-h or --help** Is used to display the help text that shows all the arguments and usage
+* **-p or --path** Is used to set the path of the project and falls back to the current directory if empty
+* **-g or --git**  Is used to specify the git repo url for the project
+* **-q or --quite** Is used to suppress all output and prompts from the script except for errors
+* **-e or --editor** Is used to set which code editor will be opened once the script has completed
+* **-l or -lang or --language** Is used to specify in which language the project will be built in so that it can run default setups (such as npm init) 
+
+I created this script in hopes of improving my knowledge of shell scripting and to help me with the boring repetitive task I had to do when starting a new project.
+
+If you have any suggestions, feedback or improvements leave a comment or create a pr on GitHub. 
+
+You can find the whole source for the script @ [Github](https://github.com/gigili/create-project) released under GPL-3.0 licence
+
+{% github gigili/create-project no-readme %}
--- create-project.sh
+++ create-project.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
+# 			GNU GENERAL PUBLIC LICENSE
+#			  Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+#title           :create-project
+#description     :This script will create all the folders, the database and open the specified code editor when starting a new project
+#author		 	 :Igor Ilić
+#website		 :https://igorilic.net
+#twitter		 :https://twitter.com/Gac_BL
+#date            :06-06-2020
+#version         :0.1
+#usage		     :create-project.sh <project-name> [-h | -p | -g | -e | -l | -q]
+
+slugify() {
+	slug=$(echo "$1" | iconv -t ascii//TRANSLIT | sed -r s/[~^]+//g | sed -r s/[^a-zA-Z0-9]+/_/g | sed -r s/^-+\|-+\$//g | tr "[:upper:]" "[:lower:]")
+	echo "$slug"
+}
+
 #clear
 
-PROJECT_NAME="$1"                       # first argument passed to the script is the name of the project
-PROJECT_PATH="${2:-$PWD}/$PROJECT_NAME" # use current dir for the path
+#MySQL Login info
+MYSQL_USER="root"
+MYSQL_PASSWORD="toor"
+VIRTUAL_HOST_PATH="/etc/httpd/conf/extra/sites-enabled"
+RESTART_SERVICE="systemctl restart httpd"
+
+PROJECT_NAME="$1"                          # first argument passed to the script is the name of the project
+SL_PROJECT_NAME=$(slugify "$PROJECT_NAME") # url friendly version of the project name
+PROJECT_PATH="${2:-$PWD}/$SL_PROJECT_NAME" # use current dir for the path
+
+GIT_REPO=""
+CODE_EDITOR="phpstorm" # change the default value to your preference
+PROJECT_LANG=""
+USE_SILENT=false
+VHOST_URL=""
 
 # Check to make sure the project name is not empty
 if [[ -z "$PROJECT_NAME" ]]; then
@@ -11,49 +45,52 @@ if [[ -z "$PROJECT_NAME" ]]; then
 	abort
 fi
 
-#MySQL Login info
-MYSQL_USER="root"
-MYSQL_PASSWORD="toor"
-
-GIT_REPO="$3" # the third argument passed to the script should be a git repo url
-
-USE_SILENT=false
-
 setup_flags() {
 	while [[ $# -gt 0 ]]; do
 		opt="$1"
 
-		shift
-
-		current_arg="$1"
-
-		if [[ "$current_arg" =~ ^-{1,2}.* ]]; then
-			log "You may have left an argument blank. Double check your command." "warning"
-		fi
-
 		case "$opt" in
 		"-h" | "--help")
-			echo "This should display help for screen for this script "
+			echo "Usage: create-project.sh <project-name> [-h | -p | -g | -e | -l | -q]"
+			echo "Options:"
+			echo "-h, --help   			Displays this help text"
+			echo "-p, --path   			Sets the path of where the project should be created"
+			echo "-g, --git    			Sets the git repository url"
+			echo "-q, --quite			Runs the script without any output or prompt to the users except errors"
+			echo "-e, --editor			Sets the code editor to be opened once the script is done"
+			echo "-l, -lang, --language		Sets the programming language used for the project and runs language specific steps to initialize everything"
+
+			exit 0
 			;;
-		"-p" | "--project")
-			PROJECT_NAME="$1"
+		"-p" | "--path")
+			shift
+			PROJECT_PATH="$1"
 
 			if [[ -n "$PROJECT_PATH" ]]; then
-				PROJECT_PATH="$PWD/$PROJECT_NAME"
+				PROJECT_PATH="$PROJECT_PATH/$SL_PROJECT_NAME"
+			else
+				PROJECT_PATH="$PWD/$SL_PROJECT_NAME"
 			fi
 			shift
 			;;
-		"-pp" | "--path")
-			PROJECT_PATH="$1/$PROJECT_NAME"
-			shift
-			;;
 		"-g" | "--git")
-			echo "USE GIT FOR THIS PROJECT"
-				GIT_REPO="$1"
+			shift
+			GIT_REPO="$1"
 			shift
 			;;
 		"-q" | "--quite")
+			shift
 			USE_SILENT=true
+			shift
+			;;
+		"-e" | "--editor")
+			shift
+			CODE_EDITOR="$1"
+			shift
+			;;
+		"-l" | "-lang" | "--language")
+			shift
+			PROJECT_LANG="$1"
 			shift
 			;;
 		*) shift ;;
@@ -64,7 +101,7 @@ setup_flags() {
 log() {
 	color=""
 	prefix="[INFO]"
-	lvl=$2
+	lvl=${2:-"info"}
 
 	if [[ "$lvl" == "success" ]]; then
 		color="\e[92m"
@@ -128,35 +165,131 @@ create_new_project() {
 		log "Created README.md" "success"
 	fi
 
-	if [ "$USE_GIT" = true ]; then
+	# make sure git exists before using it
+	if [[ -z $(type -p "git") ]]; then
 		git init "$PROJECT_PATH" >/dev/null 2>&1
-		log "Initialized git repo in $PROJECT_PATH" "log"
+		log "Initialized empty git repo in $PROJECT_PATH" "log"
 
 		if [[ -n "$GIT_REPO" ]]; then
 			git remote add origin "$GIT_REPO"
-			log "Added repository origin url" "info"
+			log "Added repository origin url: $GIT_REPO" "info"
 		fi
 	fi
 
-	create_database
-
-	open_editor
+	create_database                # Create database for the project
+	setup_virtual_host             # Setup virtual host route for the project
+	setup_language_specific_config # Initialize specific default folders based on the selected project language
+	open_editor                    # Open the specified code editor or fallback to the default one
 }
 
 create_database() {
+	if [[ -z $(type -p "mysql") ]]; then
+		log "MySQL is not installed. Skipping database creation" "warning"
+		return
+	fi
+
 	mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" <<EOFMYSQL
-				DROP DATABASE IF EXISTS \`$PROJECT_NAME\`;
-				CREATE DATABASE IF NOT EXISTS \`$PROJECT_NAME\`;
+		DROP DATABASE IF EXISTS \`$SL_PROJECT_NAME\`;
+		CREATE DATABASE IF NOT EXISTS \`$SL_PROJECT_NAME\`;
 EOFMYSQL
 
-	log "Created new MySQL database: $PROJECT_NAME" "info"
+	log "Created new MySQL database: $SL_PROJECT_NAME" "success"
+}
+
+setup_language_specific_config() {
+	if [[ -z "$PROJECT_LANG" ]]; then
+		PROJECT_LANG="php"
+	fi
+
+	case "$PROJECT_LANG" in
+	php)
+		if [[ -z $(type -p "composer") ]]; then
+			log "Composer is not installed. Skipping setup." "warning"
+			return
+		fi
+
+		composer init "$PROJECT_PATH" --name="$PROJECT_NAME" --type="project" --homepage="$VHOST_URL" --repository="$GIT_REPO"
+		;;
+	node)
+		if [[ -z $(type -p "node") ]]; then
+			log "Node is not installed. Skipping setup." "warning"
+			return
+		fi
+
+		npm init "$PROJECT_PATH" --yes
+		;;
+	*) return ;;
+	esac
 }
 
 open_editor() {
-	# phpstorm "$PROJECT_PATH"
-	#$("$EDTIOR $PROJECT_PATH")
-	subl $PROJECT_PATH
+	if [[ -z "$CODE_EDITOR" ]]; then
+		CODE_EDITOR="phpstorm"
+	fi
+
+	if [[ -z $(type -p "$CODE_EDITOR") ]]; then
+		log "Unable to find the executable for the code editor: $CODE_EDITOR" "error"
+		return
+	fi
+
+	log "Running $CODE_EDITOR $PROJECT_PATH" "info"
+	"$CODE_EDITOR" "$PROJECT_PATH" >/dev/null 2>&1
+
 }
+
+setup_virtual_host() {
+	log "Setup new virtual host for project $PROJECT_NAME?" "info"
+
+	if ! ask_for_confirmation; then
+		log "Skipping virtual host creation" "info"
+		return
+	fi
+
+	log "Creating new virtual host entry for $PROJECT_NAME" "info"
+
+	echo -n -e "\e[37mProject url:\e[0m "
+	read -r
+	echo
+
+	VHOST_URL="$REPLY"
+
+	VH_FILE="<VirtualHost *:80>
+		ServerAdmin admin@example.com
+		DocumentRoot \"$PROJECT_PATH\"
+		ServerName \"$VHOST_URL\"
+		ServerAlias \"www.$VHOST_URL\"
+		ErrorLog \"$PROJECT_PATH/logs/error.log\"
+		CustomLog \"$PROJECT_PATH/logs/access.log\" common
+		<Directory \"$PROJECT_PATH\">
+			AllowOverride All
+		</Directory>
+	</VirtualHost>"
+
+	if [[ -d "$VIRTUAL_HOST_PATH" ]]; then
+		log "Invalid path to the virtual hosts config folder." "error"
+		return
+	fi
+
+	if [[ $(echo "$VH_FILE" | sudo tee -a "$VIRTUAL_HOST_PATH/$SL_PROJECT_NAME.conf") ]]; then
+		log "Virtual host for $PROJECT_NAME created successfully" "success"
+
+		if [[ $(echo "127.0.0.1 $VHOST_URL" | sudo tee -a "/etc/hosts") ]] && [[ $(echo "127.0.0.1 www.$VHOST_URL" | sudo tee -a "/etc/hosts") ]]; then
+			log "Added $VHOST_URL to the hosts file" "success"
+		fi
+
+		log "Restarting apache" "info"
+
+		if [[ $(sudo "$RESTART_SERVICE" >/dev/null 2>&1) ]]; then
+			log "Apache service restarted" "success"
+		else
+			log "Unable to restart apache" "warning"
+		fi
+	fi
+}
+
+if [[ -z "$PROJECT_PATH" ]] || [[ ! $(readlink -f "$PROJECT_PATH" >/dev/null 2>&1) ]]; then
+	PROJECT_PATH="$PWD/$SL_PROJECT_NAME"
+fi
 
 setup_flags "$@"
 
@@ -180,3 +313,5 @@ if ask_for_confirmation; then
 else
 	abort
 fi
+
+echo "writing"
